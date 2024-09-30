6e7b6a99d5a701edb774b98598e3c6eb03423dc1
Piotr K. Wyrwas <pw.piotr.wyrwas@gmail.com>
2024-09-30 18:55:43 +0200

    Set version to match newest ANTG release, make the MDK lib compatible with newest ANTG spec

--- src/main/kotlin/org/medaware/avis/MedawareDesignKit.kt
+++ src/main/kotlin/org/medaware/avis/MedawareDesignKit.kt
@@ -1,11 +1,12 @@
 package org.medaware.avis
 
+import org.medaware.anterogradia.runtime.Runtime
 import org.medaware.anterogradia.runtime.library.AnterogradiaLibrary
 import org.medaware.anterogradia.runtime.library.DiscreteFunction
 import org.medaware.anterogradia.runtime.library.StateRetention.STATELESS
 
 @AnterogradiaLibrary(STATELESS)
-class MedawareDesignKit {
+class MedawareDesignKit(val runtime: Runtime) {
 
     @DiscreteFunction(identifier = "about")
     fun about(): String = "Medaware Design Kit\n{C} Medaware 2024\n"
