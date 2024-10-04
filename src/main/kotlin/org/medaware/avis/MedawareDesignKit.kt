28afafc95e1993b94d06c24c8bf01231c29ed6ff
Piotr K. Wyrwas <pw.piotr.wyrwas@gmail.com>
2024-10-04 13:49:36 +0200

    Add basic formatting utilities

--- src/main/kotlin/org/medaware/avis/MedawareDesignKit.kt
+++ src/main/kotlin/org/medaware/avis/MedawareDesignKit.kt
@@ -4,11 +4,47 @@ import org.medaware.anterogradia.runtime.Runtime
 import org.medaware.anterogradia.runtime.library.AnterogradiaLibrary
 import org.medaware.anterogradia.runtime.library.DiscreteFunction
 import org.medaware.anterogradia.runtime.library.StateRetention.STATELESS
+import org.medaware.anterogradia.syntax.Node
 
 @AnterogradiaLibrary(STATELESS)
 class MedawareDesignKit(val runtime: Runtime) {
 
     @DiscreteFunction(identifier = "about")
     fun about(): String = "Medaware Design Kit\n{C} Medaware 2024\n"
+
+    @DiscreteFunction(identifier = "root", params = ["body"])
+    fun root(body: Node): String {
+        return """
+            <div class="tan-article">${body.evaluate(runtime)}</div>
+        """.trimIndent()
+    }
+
+    @DiscreteFunction(identifier = "heading", params = ["value"])
+    fun heading(value: Node): String {
+        return """
+            <h1>${value.evaluate(runtime)}</h1>
+        """.trimIndent()
+    }
+
+    @DiscreteFunction(identifier = "img", params = ["src"])
+    fun img(src: Node): String {
+        return """
+            <img src="${src.evaluate(runtime)}" class="tan-img"></img>
+        """.trimIndent()
+    }
+
+    @DiscreteFunction(identifier = "subheading", params = ["value"])
+    fun subheading(value: Node): String {
+        return """
+            <h2>${value.evaluate(runtime)}</h2>
+        """.trimIndent()
+    }
+
+    @DiscreteFunction(identifier = "lead", params = ["value"])
+    fun lead(value: Node): String {
+        return """
+            <p class="tan-lead">${value.evaluate(runtime)}</p>
+        """.trimIndent()
+    }
 
 }
